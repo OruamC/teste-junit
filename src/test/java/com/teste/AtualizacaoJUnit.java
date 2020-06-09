d914dbbd4b2ff2ba7af67c9f2d633c8fa719bdc9
mauro <mauro@mauro.com>
2020-06-09 14:30:46 -0300

    Introducao a TDD e JUnit 5

---
+++ src/test/java/com/teste/AtualizacaoJUnit.java
@@ -0,0 +1,54 @@
+package com.teste;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.*;
+
+public class AtualizacaoJUnit {
+
+    @Test
+    void lambdaExpressions() {
+        assertTrue(Stream.of(1, 2, 3)
+                .mapToInt(i -> i)
+                .sum() > 5, () -> "Sum should be greater than 5");
+    }
+
+
+
+    @Test
+    void groupAssertions() {
+        int[] numbers = {0, 1, 2, 3, 4};
+        assertAll("numbers",
+                () -> assertEquals(numbers[0], 0),
+                () -> assertEquals(numbers[3], 3),
+                () -> assertEquals(numbers[4], 4)
+        );
+    }
+
+    @Test
+    void trueAssumption() {
+        assumeTrue(5 > 1);
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    void falseAssumption() {
+        assumeFalse(5 < 1);
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    void assumptionThat() {
+        String someString = "Just a string";
+        assumingThat(
+                someString.equals("Just a string"),
+                () -> assertEquals(2 + 2, 4)
+        );
+    }
+}
