02a1f5f5ec0d8a4d9ee6701f615be0e72f44f49c
mauro <mauro@mauro.com>
2020-06-09 12:12:29 -0300

    Utilizando Matchers e AssertJ

---
+++ src/main/java/com/teste/Person.java
@@ -0,0 +1,28 @@
+package com.teste;
+
+public class Person {
+
+    String nome;
+    String sobrenome;
+
+    public Person(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
